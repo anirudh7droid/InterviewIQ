aa9f45c08bbb1ddedc950a119206e223893f1d72
anirudh7droid <anirudhnadigoti7@gmail.com>
2026-07-14 16:47:33 +0530

    Milestone 1: Resume upload and PDF parsing

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import pdfplumber
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+
+            if page_text:
+                text += page_text + "\n"
+
+    return text
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    if "resume" not in request.files:
+        return "No file uploaded"
+
+    file = request.files["resume"]
+
+    if file.filename == "":
+        return "No file selected"
+
+    filepath = os.path.join(
+        app.config["UPLOAD_FOLDER"],
+        file.filename
+    )
+
+    file.save(filepath)
+
+    extracted_text = extract_text_from_pdf(filepath)
+
+    return f"""
+    <h2>Resume Text Extracted Successfully</h2>
+    <pre>{extracted_text}</pre>
+    """
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
